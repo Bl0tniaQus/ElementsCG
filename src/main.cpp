2d23f6bda8be93876661b41c0f05d762d66382d4
Bl0tniaQus <focusedmud@wp.pl>
2024-07-22 21:56:50 +0200

    windows deployment tested

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,10 @@
 #include <iostream>
 #include <QApplication>
 #include <QDebug>
-#include <QStyleFactory>
 int main(int argc, char *argv[])
 {
     srand(time(NULL));
-    //QLibraryInfo();
-    //Dark palette from
-    //https://gist.github.com/QuantumCD/6245215
-    QApplication::setStyle(QStyleFactory::create("Fusion"));
-    /*QPalette darkPalette;
-    darkPalette.setColor(QPalette::Window, QColor(53,53,53));
-    darkPalette.setColor(QPalette::WindowText, Qt::white);
-    darkPalette.setColor(QPalette::Base, QColor(25,25,25));
-    darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
-    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
-    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
-    darkPalette.setColor(QPalette::Text, Qt::white);
-    darkPalette.setColor(QPalette::Button, QColor(53,53,53));
-    darkPalette.setColor(QPalette::ButtonText, Qt::white);
-    darkPalette.setColor(QPalette::BrightText, Qt::red);
-    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
-    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
-    darkPalette.setColor(QPalette::HighlightedText, Qt::black);*/
     QApplication a(argc, argv);
-    //a.setPalette(darkPalette);
     MainWindow w;
    // Duel duel;
    // Deck deck(1);
